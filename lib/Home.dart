0fd49dd643fcfcf4784994cce830e2803206fe1a
Alexander Adimas <alexanderadimas@gmail.com>
2020-08-11 22:08:14 +0700

    update

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:scoped_model/scoped_model.dart';
-//import 'package:search_widget/search_widget.dart';
+import 'package:search_widget/search_widget.dart';
 import 'Cart.dart';
 import 'ScopeManage.dart';
 import 'Details.dart';
