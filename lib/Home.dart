f53b343402f8089bcbb70d644cfe3a9b5f53c667
Alexander Adimas <alexanderadimas@gmail.com>
2020-08-18 17:05:43 +0700

    update

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:scoped_model/scoped_model.dart';
-import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'Cart.dart';
 import 'ScopeManage.dart';
 import 'Details.dart';
@@ -23,9 +22,31 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
+  Icon searchIcon = Icon(
+    Icons.search,
+    color: Colors.orangeAccent,
+  );
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+  final TextEditingController searchQuery = TextEditingController();
+  List<Data> data;
+  List<Data> searchList = List();
+
+  bool isSearching;
+  String searchText = '';
+  HomeState() {
+    searchQuery.addListener(() {
+      if (searchQuery.text.isEmpty) {
+        setState(() {
+          isSearching = false;
+          searchText = '';
+        });
+      }
+    });
+  }
+
   Widget gridGenerate(List<Data> data, aspectRatio) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10.0),
+      padding: EdgeInsets.all(5.0),
       child: GridView.builder(
         shrinkWrap: true,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -41,13 +62,15 @@ class HomeState extends State<Home> {
                           builder: (context) => Details(detail: data[index])));
                 },
                 child: Container(
-                    height: 300.0,
                     padding: EdgeInsets.all(5.0),
                     decoration: BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.rectangle,
-                        border: Border.all(color: Colors.orange[200]),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                      color: Colors.white,
+                      shape: BoxShape.rectangle,
+                      border: Border.all(color: Colors.orange[200]),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
@@ -55,27 +78,27 @@ class HomeState extends State<Home> {
                           child: ClipRRect(
                             borderRadius: BorderRadius.all(Radius.circular(10)),
                             child: Image(
+                              fit: BoxFit.cover,
                               image: NetworkImage(
                                   'http://www.malmalioboro.co.id/${data[index].gambar}'),
-                              fit: BoxFit.fill,
                             ),
                           ),
                         ),
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                            '${data[index].nama}',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w500),
-                          ),
+                        Text(
+                          '${data[index].nama}',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w700),
                         ),
                         Padding(
-                          padding: EdgeInsets.only(right: 10),
+                          padding: EdgeInsets.symmetric(horizontal: 5),
                           child: Column(children: <Widget>[
-                            Divider(color: Colors.black),
+                            Divider(
+                              color: Colors.orange[200],
+                              thickness: 1,
+                            ),
                             Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
+                              //mainAxisAlignment: MainAxisAlignment.center,
                               children: <Widget>[
                                 Text(
                                   NumberFormat.currency(
@@ -102,6 +125,9 @@ class HomeState extends State<Home> {
 
   Widget cartButton() {
     return FloatingActionButton(
+      elevation: 0,
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.orangeAccent,
       onPressed: () {
         Navigator.push(
             context, MaterialPageRoute(builder: (context) => Cart()));
@@ -109,15 +135,15 @@ class HomeState extends State<Home> {
       child: Stack(
         children: <Widget>[
           Padding(
-            padding: EdgeInsets.only(right: 10, left: 10, top: 10, bottom: 10),
+            padding: EdgeInsets.all(10),
             child: Icon(
               Icons.shopping_cart,
-              size: 25,
+              size: 30,
             ),
           ),
           Positioned(
             top: 5,
-            left: 25,
+            left: 30,
             child: ScopedModelDescendant<AppModel>(
               builder: (context, child, model) {
                 return Container(
@@ -146,7 +172,6 @@ class HomeState extends State<Home> {
           ),
         ],
       ),
-      backgroundColor: Colors.orangeAccent,
     );
   }
 
@@ -155,13 +180,14 @@ class HomeState extends State<Home> {
     var size = MediaQuery.of(context).size;
 
     /*24 is for notification bar on Android*/
-    final double itemHeight = (size.height - kToolbarHeight - 24) / 3;
+    final double itemHeight = (size.height - kToolbarHeight + 50) / 3.5;
     final double itemWidth = size.width / 3;
 
     // TODO: implement build
     return WillPopScope(
       onWillPop: () => Future.value(false),
       child: Scaffold(
+        key: scaffoldKey,
         appBar: AppBar(
           elevation: 1,
           automaticallyImplyLeading: false,
@@ -173,86 +199,34 @@ class HomeState extends State<Home> {
             ),
           ),
           actions: <Widget>[
-            Stack(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 15, right: 20, left: 15),
-                  child: InkResponse(
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => Cart()));
-                    },
-                    child: Icon(
-                      Icons.shopping_cart,
-                      size: 25,
-                      color: Colors.orangeAccent,
-                    ),
-                  ),
+            Padding(
+              padding: EdgeInsets.only(right: 15),
+              child: InkResponse(
+                onTap: () {},
+                child: Icon(
+                  Icons.search,
+                  size: 30,
+                  color: Colors.orangeAccent,
                 ),
-                Positioned(
-                  top: 10,
-                  right: 15,
-                  child: ScopedModelDescendant<AppModel>(
-                    builder: (context, child, model) {
-                      return Container(
-                        //padding: EdgeInsets.all(1),
-                        decoration: new BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        constraints: BoxConstraints(
-                          minWidth: 15,
-                          minHeight: 15,
-                        ),
-                        child: Text(
-                          (model.cartListing.length > 0)
-                              ? model.cartListing.length.toString()
-                              : '0',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 12,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
+              ),
             ),
           ],
         ),
         body: ScopedModelDescendant<AppModel>(builder: (context, child, model) {
-          return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 10,
-                            spreadRadius: 5)
-                      ],
-                    ),
-                    height: 0,
-                    child: null),
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(color: Colors.white),
-                    child: gridGenerate(
-                        model.itemListing, (itemWidth / itemHeight)),
-                  ),
-                ),
-              ]);
+          return Container(
+            decoration: BoxDecoration(color: Colors.white),
+            child: gridGenerate(model.itemListing, (itemWidth / itemHeight)),
+          );
         }),
-        //floatingActionButton: cartButton(),
-        bottomNavigationBar: BottomAppBar(
-            elevation: 1,
-            child: Container(
-              height: 5,
-            )),
+        floatingActionButton: Container(
+          margin: EdgeInsets.only(right: 5, bottom: 5),
+          width: 70,
+          height: 70,
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black12, width: 3),
+              shape: BoxShape.circle),
+          child: cartButton(),
+        ),
       ),
     );
   }
