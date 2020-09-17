2f2d6f27d117dac578ddc48e820810dfed91b005
dexdim <alexanderadimas@gmail.com>
2020-09-17 17:01:24 +0700

    update

--- lib/auth/Signup.dart
+++ lib/auth/Signup.dart
@@ -16,6 +16,62 @@ class SignupState extends State<Signup> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+        return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+              Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text("Meet Up",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                      fontFamily: 'Roboto')),
+            ),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SignInButton(
+                  Buttons.Email,
+                  text: "Sign up with Email",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EmailSignUp()),
+                    );
+                  },
+                )),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SignInButton(
+                  Buttons.Google,
+                  text: "Sign up with Google",
+                  onPressed: () {},
+                )),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: SignInButton(
+                  Buttons.Twitter,
+                  text: "Sign up with Twitter",
+                  onPressed: () {},
+                )),
+            Padding(
+                padding: EdgeInsets.all(10.0),
+                child: GestureDetector(
+                    child: Text("Log In Using Email",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue)),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => EmailLogIn()),
+                      );
+                    }))
+          ]),
+        ))
+
   }
 }
